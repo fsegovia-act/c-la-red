a0411075892dad051fe5d704973afcc6addd6c4d
fsegovia-act <fsegovia.act@gmail.com>
2025-04-13 16:54:31 -0300

    resolve conflicts with untracked files

---
+++ app/api/_lib/mongodb.ts
@@ -0,0 +1,53 @@
+// lib/mongodb.ts
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+/**
+ * Define interface for mongoose cache
+ */
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+/**
+ * Global type declaration for the mongoose cache
+ */
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+/**
+ * Cached connection for MongoDB.
+ */
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+async function dbConnect(): Promise<typeof mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI as string);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
