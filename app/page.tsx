509f0c9bd45cdf138b4e1ba99819c4fa33fac040
fsegovia-act <fsegovia.act@gmail.com>
2025-04-23 10:19:14 -0300

    add navigation pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
 "use client";
 
+import Link from "next/link";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6">Home Page (public)</h1>
+      <h5 className="text-1xl font-bold mb-3 mt-3">Private Pages</h5>
+      <ul>
+        <li>
+          <Link href="/product/info/67fbcb36df0604fa89fbed66">Info Page</Link>
+        </li>
+        <li>
+          <Link href="/product/edit/67fbcb36df0604fa89fbed66">
+            Edit Product Page
+          </Link>
+        </li>
+        <li>
+          <Link href="/product/create">Create Product Page</Link>
+        </li>
+        <li>
+          <Link href="/products/management">Product Management Page</Link>
+        </li>
+        <li>
+          <Link href="/settings">Settings Page</Link>
+        </li>
+      </ul>
+      <h5 className="text-1xl font-bold mb-3 mt-3">Public Pages</h5>
+      <ul>
+        <li>
+          <Link href="/products">Product List Page</Link>
+        </li>
+        <li>
+          <Link href="/product/details/CODE002">Product Details Page</Link>
+        </li>
+        <li>
+          <Link href="/sign-in">Sign In Page</Link>
+        </li>
+      </ul>
     </div>
   );
 };
