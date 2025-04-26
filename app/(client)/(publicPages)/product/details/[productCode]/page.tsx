b985fc96ba66a8affd364915018812eeb25a2144
fsegovia-act <fsegovia.act@gmail.com>
2025-04-26 01:41:05 -0300

    fix styles on products pages

--- app/(client)/(publicPages)/product/details/[productCode]/page.tsx
+++ app/(client)/(publicPages)/product/details/[productCode]/page.tsx
@@ -76,7 +76,7 @@ export default function ProductDetailsByCodePage() {
     <div className="flex flex-col justify-between min-h-screen">
       <MainNavigationBar />
 
-      <div className="max-w-6xl mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-6">
           Product Details Page (public)
         </h1>
