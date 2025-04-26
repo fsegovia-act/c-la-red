4ec120975a15e18906f120d4e4eda86263e9710a
fsegovia-act <fsegovia.act@gmail.com>
2025-04-26 14:35:39 -0300

    add admin navigation bar on admin pages

--- app/(client)/(privatePages)/product/edit/[productId]/page.tsx
+++ app/(client)/(privatePages)/product/edit/[productId]/page.tsx
@@ -5,6 +5,7 @@ import { useParams, useRouter } from "next/navigation";
 import { Product } from "../../../../_lib/interfaces";
 import InfoProduct from "../../../../_components/product/info";
 import EditProduct from "../../../../_components/product/edit";
+import AdminNavigationBar from "../../../../_components/navigation/adminNavigationBar";
 
 export default function EditProductPage() {
   const params = useParams();
@@ -73,19 +74,34 @@ export default function EditProductPage() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Edit Product Page (Private)</h1>
-      {typeAction === "edit" && (
-        <EditProduct
-          product={product}
-          fetchProduct={fetchProduct}
-          setIsLoading={setIsLoading}
-          setError={setError}
-          isLoading={isLoading}
-          setTypeAction={setTypeAction}
-        />
-      )}
-      {typeAction === "info" && <InfoProduct product={product} />}
-    </div>
+    <>
+      <AdminNavigationBar />
+
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold mb-6">
+            Edit Product Page (Private)
+          </h1>
+
+          <button
+            onClick={() => router.push(`/product/info/${product._id}`)}
+            className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-300 hover:cursor-pointer max-h-[40px] hover:cursor-pointer"
+          >
+            Info Product
+          </button>
+        </div>
+        {typeAction === "edit" && (
+          <EditProduct
+            product={product}
+            fetchProduct={fetchProduct}
+            setIsLoading={setIsLoading}
+            setError={setError}
+            isLoading={isLoading}
+            setTypeAction={setTypeAction}
+          />
+        )}
+        {typeAction === "info" && <InfoProduct product={product} />}
+      </div>
+    </>
   );
 }
