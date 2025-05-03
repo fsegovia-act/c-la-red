b9703ba0a2e8b2b7c19823ba016b7c415f36e521
fsegovia-act <fsegovia.act@gmail.com>
2025-05-03 02:52:00 -0300

    update number of results per pages

--- app/api/_helpers/pagination.ts
+++ app/api/_helpers/pagination.ts
@@ -1,7 +1,7 @@
 export function getPaginationParams(url: string) {
   const { searchParams } = new URL(url);
   const page = parseInt(searchParams.get("page") || "1", 10);
-  const limit = parseInt(searchParams.get("limit") || "50", 10);
+  const limit = parseInt(searchParams.get("limit") || "20", 10);
   const skip = (page - 1) * limit;
     
   return { page, limit, skip };
