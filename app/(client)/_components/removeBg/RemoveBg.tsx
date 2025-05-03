27107364e775e3529b6a0b3d79949dff00eed9d0
fsegovia-act <fsegovia.act@gmail.com>
2025-05-03 01:40:13 -0300

    add remove bg image component

--- app/(client)/_components/removeBg/RemoveBg.tsx
+++ app/(client)/_components/removeBg/RemoveBg.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import { useState, ChangeEvent } from "react";
+import Image from "next/image";
+import { useState, ChangeEvent, Dispatch, SetStateAction } from "react";
+import { base64ToFile } from "../../_lib/helpers";
+import { defaultImageUrl } from "../../_lib/constant";
 
-export default function BackgroundRemover() {
-  const [originalImage, setOriginalImage] = useState<string | null>(null);
-  const [processedImage, setProcessedImage] = useState<string | null>(null);
+const NEXT_PUBLIC_S3_BASE_URL = process.env.NEXT_PUBLIC_S3_BASE_URL;
+
+export default function BackgroundRemover({
+  file,
+  setFile,
+  imageUrl = defaultImageUrl,
+}: {
+  file: File | null;
+  setFile: Dispatch<SetStateAction<File | null>>;
+  imageUrl?: string;
+}) {
+  const [defautImage, setDefaultImage] = useState<boolean>(true);
+  const [sourceImage, setSourceImage] = useState<string>(`${NEXT_PUBLIC_S3_BASE_URL}${imageUrl}`);
+  const [processedImage, setProcessedImage] = useState<boolean>(false);
+  const [fileName, setFileName] = useState<string>("image-product-default.jpg");
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
@@ -12,14 +27,17 @@ export default function BackgroundRemover() {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setOriginalImage(reader.result as string);
+        setSourceImage(reader.result as string);
       };
       reader.readAsDataURL(file);
+      setFileName(file.name);
+      setDefaultImage(false);
     }
   };
 
   const removeBackground = async () => {
-    if (!originalImage) return;
+    if (defautImage) return;
+    if (processedImage) return;
 
     setLoading(true);
 
@@ -30,22 +48,42 @@ export default function BackgroundRemover() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          image: originalImage.split(",")[1],
+          image: sourceImage.split(",")[1],
         }),
       });
 
       const data: { image: string } = await response.json();
-      setProcessedImage(`data:image/png;base64,${data.image}`);
+      const base64Image = `data:image/png;base64,${data.image}`;
+      const fileObject = base64ToFile(base64Image, fileName);
+
+      setSourceImage(base64Image);
+      setProcessedImage(true);
+      setFile(fileObject);
     } catch (error) {
-      console.error("Error:", error);
+      console.log("Error:", error);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">Eliminador de fondos</h1>
+    <>
+      <div>
+        <label
+          htmlFor="Image"
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          {processedImage ? "Imagen sin fondo:" : "Imagen original:"}
+        </label>
+      </div>
+      <div className="relative h-96 w-full rounded-lg overflow-hidden">
+        <Image
+          src={sourceImage}
+          alt={file ? file.name : "image-product-default"}
+          fill
+          className="object-cover"
+        />
+      </div>
 
       <input
         type="file"
@@ -54,29 +92,17 @@ export default function BackgroundRemover() {
         className="w-full px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 disabled:opacity-50"
       />
 
-      {originalImage && (
-        <button
-          onClick={removeBackground}
-          disabled={loading}
-          className="bg-blue-500 text-white px-4 py-2 mt-4 rounded"
-        >
-          {loading ? "Procesando..." : "Quitar fondo"}
-        </button>
-      )}
-
-      {originalImage && (
-        <div className="mt-6">
-          <h2 className="font-semibold mb-2">Imagen original:</h2>
-          <img src={originalImage} alt="Original" className="max-w-sm" />
+      {!processedImage && (
+        <div className="flex justify-end">
+          <button
+            onClick={removeBackground}
+            disabled={loading}
+            className="bg-blue-500 text-white px-4 py-2 mt-4 rounded"
+          >
+            {loading ? "Procesando..." : "Quitar fondo"}
+          </button>
         </div>
       )}
-
-      {processedImage && (
-        <div className="mt-6">
-          <h2 className="font-semibold mb-2">Imagen sin fondo:</h2>
-          <img src={processedImage} alt="Sin fondo" className="max-w-sm" />
-        </div>
-      )}
-    </div>
+    </>
   );
 }
