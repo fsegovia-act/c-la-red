d53d6515b6230341e9ea55af2bc0a87f127ba3cf
fsegovia-act <fsegovia.act@gmail.com>
2025-05-14 10:25:31 -0300

    correct phone number constant

--- app/(client)/_lib/constant.ts
+++ app/(client)/_lib/constant.ts
@@ -1,7 +1,7 @@
 export const BUSINESS_NAME: string = "C La Red";
 export const BUSINESS_ADDRESS: string = "Jorge Odiard 2131";
 export const BUSINESS_EMAIL: string = "cyberlaredconcordia@mail.com";
-export const BUSINESS_PHONE_NUMBER: string = "+5493456260360";
+export const BUSINESS_PHONE_NUMBER: string = "+549 345 6 26 03 60";
 
 export const PUBLIC = "public";
 export const PRIVATE = "private";
