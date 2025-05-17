a4f3a84a8d3b5964ba79f60b1f94d91ce7bc3c88
fsegovia-act <fsegovia.act@gmail.com>
2025-05-17 02:24:30 -0300

    add api interfaces

---
+++ app/api/_lib/interfaces.ts
@@ -0,0 +1,28 @@
+export const UserRole = {
+  ADMIN: "admin",
+  SUPER_ADMIN: "super_admin",
+} as const;
+
+export const { ADMIN, SUPER_ADMIN } = UserRole;
+
+export type UserRole = (typeof UserRole)[keyof typeof UserRole];
+
+export interface CreateUserInput {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  profileImageUrl?: string;
+  role?: UserRole;
+}
+
+export interface UpdateUserInput {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phoneNumber?: string;
+  profileImageUrl?: string;
+  isActive?: boolean;
+  role?: UserRole;
+}
