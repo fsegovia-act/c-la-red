4c3ce48f2974f3e7de61daad229d3da290ccd920
fsegovia-act <fsegovia.act@gmail.com>
2025-05-28 09:51:36 -0300

    fix loader on product pages

--- app/(client)/(publicPages)/products/page.tsx
+++ app/(client)/(publicPages)/products/page.tsx
@@ -6,9 +6,11 @@ import { Product } from "../../_lib/interfaces";
 import MainNavigationBar from "../../_components/navigation/mainNavigationBar";
 import MainFooter from "../../_components/footer/mainFooter";
 import { useSearchParams } from "next/navigation";
-import Loader from "../../_components/loader/Loader";
 import { PUBLIC } from "../../_lib/constant";
 import InfiniteScrollProducts from "../../_components/product/infiniteScrollProducts";
+import Loader from "../../_components/loader/Loader";
+import ErrorComponent from "../../_components/error/Error";
+import ProductNotFound from "../../_components/product/notFound";
 
 const Products: NextPage = () => {
   const [initialProducts, setInitialProducts] = useState<Product[]>([]);
@@ -16,7 +18,7 @@ const Products: NextPage = () => {
   const searchParams = useSearchParams();
   const search = searchParams.get("search");
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -25,7 +27,7 @@ const Products: NextPage = () => {
 
   const getProducts = async (page = 1): Promise<Product[]> => {
     try {
-      setIsLoading(true);
+      if (page === 1) setIsLoading(true);
       setError(null);
       const res = search
         ? await fetch(
@@ -43,7 +45,7 @@ const Products: NextPage = () => {
       setError("Error connecting to the server");
       return [];
     } finally {
-      setIsLoading(false);
+      if (page === 1) setIsLoading(false);
     }
   };
 
@@ -53,6 +55,8 @@ const Products: NextPage = () => {
   const fetchMoreProducts = async (page: number): Promise<Product[]> =>
     await getProducts(page);
 
+  if (error) return <ErrorComponent error={error} />;
+
   return (
     <>
       <div className="flex flex-col justify-between min-h-screen">
@@ -61,20 +65,16 @@ const Products: NextPage = () => {
         <div className="container mx-auto px-4 py-8">
           <h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
 
-          {error && (
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
-              {error}
-            </div>
-          )}
+          {isLoading && <Loader />}
 
-          {initialProducts.length ? (
+          {!isLoading && initialProducts.length > 0 && (
             <InfiniteScrollProducts
               initialProducts={initialProducts}
               fetchMoreProducts={fetchMoreProducts}
             />
-          ) : (
-            <div>Product not found</div>
           )}
+
+          {!isLoading && initialProducts.length === 0 && <ProductNotFound />}
         </div>
 
         <MainFooter />
